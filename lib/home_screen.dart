9290f80be89bbf880cfeac6110e54df4e311043f
Farhana Ahmed <farhana.ahmed.fima@gmail.com>
2022-10-20 22:56:29 +0600

    Complete UI

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,67 +1,393 @@
+import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white70,
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        padding: EdgeInsets.only(
-          top: 80,
-          bottom: 80,
-          left: 30,
-          right: 30,
-        ),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      backgroundColor: Colors.white.withOpacity(0.9),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.only(
+                top: 80,
+                //bottom: 80,
+                left: 30,
+                right: 30,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Michael Safranov",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "Manager at CNN",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                      //Spacer(),
+                      Container(
+                        // color: Colors.black54,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black87,
+                          ),
+                        ),
+                        child: Image.network(
+                          "https://images.unsplash.com/photo-1544717297-fa95b6ee9643?ixlib="
+                              "rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto="
+                              "format&fit=crop&w=1169&q=80",
+                          height: 60,
+                          width: 60,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Statistics",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        "MONTH",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 12,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 35,
+                            width: 35,
+                            color: Colors.black,
+                            child: Icon(
+                              Icons.vpn_key_outlined,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            "Security attacks",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.arrow_upward_outlined,
+                            size: 16,
+                            color:Colors.teal,
+                          ),
+                          Text(
+                            " 68%",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.teal,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 35,
+                            width: 35,
+                            color: Colors.black,
+                            child: Icon(
+                              Icons.fingerprint,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            "Protected bunches",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.arrow_downward_outlined,
+                            size: 16,
+                            color: Colors.deepOrangeAccent,
+                          ),
+                          Text(
+                            " 38%",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.deepOrangeAccent,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 35,
+                            width: 35,
+                            color: Colors.black,
+                            child: Icon(
+                              Icons.view_in_ar,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            "Runtime cost",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "\$6235",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.only(
+              top: 25,
+              bottom: 80,
+              left: 30,
+              right: 30,
+            ),
+            color: Colors.white,
+            child: Column(
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      "Michael Safranov",
+                      "Company goals",
                       style: TextStyle(
-                        color: Colors.black,
                         fontSize: 24,
                         fontWeight: FontWeight.w500,
                       ),
                     ),
-                    SizedBox(
-                      height: 5,
-                    ),
                     Text(
-                      "Manager at CNN",
+                      "ALL",
                       style: TextStyle(
-                        color: Colors.grey,
+                        fontSize: 12,
                         fontWeight: FontWeight.w600,
+                        color: Colors.black87,
                       ),
                     ),
                   ],
                 ),
-                //Spacer(),
+                SizedBox(
+                  height: 20,
+                ),
                 Container(
-                  // color: Colors.black54,
-                  child: Image.network(
-                    "https://images.unsplash.com/photo-1639747280929-e84ef392c69a?ixlib"
-                        "=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto"
-                      "=format&fit=crop&w=687&q=80",
-                    height: 70,
-                    width: 70,
-                    fit: BoxFit.cover,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                    ),
                   ),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 130,
+                        decoration: BoxDecoration(
+                          border: Border(
+                            right: BorderSide(
+                              color: Colors.black87,
+                            ),
+                          ),
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Colors.teal,
+                              Colors.yellowAccent,
+                            ]
+                          ),
+                        ),
+                        child: SizedBox(
+                          width: 8,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(11.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                 Row(
+                                   children: [
+                                     // Container(
+                                     //   height: 8,
+                                     //   width: 8,
+                                     //   decoration: BoxDecoration(
+                                     //     borderRadius: BorderRadius.all(Radius.circular(15)),
+                                     //     color: Colors.black,
+                                     //   ),
+                                     // ),
+                                     Icon(
+                                       Icons.check_circle,
+                                       color: Colors.black87,
+                                       size: 12,
+                                     ),
+                                     SizedBox(
+                                       width: 5,
+                                     ),
+                                     Text(
+                                       "Cybersecurity teams",
+                                       style: TextStyle(
+                                         fontWeight: FontWeight.w500,
+                                         fontSize: 12,
+                                       ),
+                                     ),
+                                   ],
+                                 ),
 
+                                 Text(
+                                   "Active",
+                                   style: TextStyle(
+                                     color: Colors.grey,
+                                   ),
+                                 ),
+
+                                ],
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                "Conduct phising simulations within company",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Divider(
+                                color: Colors.grey,
+                                thickness: 1,
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.access_time,
+                                    color: Colors.grey,
+                                    size: 14,
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    "Ends in 24 days",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ],
-
-        ),
-
+          ),
+        ],
       ),
     );
   }
